08b0053a6ebc7c93f5cc3c5691c3f097cda3443d
Haci_of80 <Haci_of80@STHQ0123-20>
2023-03-18 13:57:03 +0400

    Add project files.

---
+++ SysProgLesson6_hw/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SysProgLesson6_hw
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _currentWord = string.Empty;
+            _acceptableWords = new();
+
+            MessageBox.Show(Directory.GetCurrentDirectory());
+
+        }
+
+        private string _currentWord;
+        private List<string> _acceptableWords { get; set; }
+        private List<string> _words { get; set; }
+
+        private void txt_baseText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btn_num_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_clear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_Add_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_confirm_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
